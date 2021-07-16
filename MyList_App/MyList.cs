b09da884c41c8bd6577d7e52d29dec459fbc8e00
Pavlov Vladimir <bob.pavlov.a7@gmail.com>
2021-07-16 16:19:53 +0300

    Create new project and class MyList<T>

---
+++ MyList_App/MyList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList_App
+{
+    class MyList<T> : IEnumerable<T>, IEnumerator<T>
+    {
+    }
+}
